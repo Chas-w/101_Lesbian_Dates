aa244fd40e16f5d80378fcfa250fd5ff55a0f3ac
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2026-06-24 22:35:12 -0400

    boob minigame

--- Scripts/Issues/issue_001.gd
+++ Scripts/Issues/issue_001.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var dialogue_controller : Control
 @export var anim_player : AnimationPlayer
+@export var minigames : Dictionary
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,7 +12,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+	
 func _play_anim(anim : String):
 	anim_player.play(anim)
 	pass #if can't progress unless animation is played
@@ -20,6 +21,9 @@ func _wait_for_gameplay(gameplay, next_status):
 	pass #if can't progress until gameplay has been finished
 #endregion
 
-func _play_game(minigame : String):
-	print(minigame)
-	pass
+func _play_game(choice : String):
+	var find = get_node(minigames[choice])
+	if (!find.start_game):
+		find.start_game = true
+	else:
+		return
--- Scripts/minigames/boob_game.gd
+++ Scripts/minigames/boob_game.gd
@@ -1,11 +1,71 @@
 extends Control
+@export var kai_boob : Sprite2D
+@export var paul_boob : Sprite2D
+@export var needle : Sprite2D
+@export var needle_max_pos : Node2D
+@export var needle_min_pos : Node2D
+@export var needle_start_pos : Node2D
+@export var pierce_pos : Node2D
 
+var start_game : bool
+var started : bool
+var pierce : bool = false
 
+var rise : bool
+
+var max_speed = 550
+var min_speed = 500
+var speed
+
+var ok_shot : bool 
+var great_shot : bool 
+
+var buffer = 1
+var wait_after_pierce = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	speed = randf_range(min_speed,max_speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(start_game):
+		if (!started):
+			needle.position = needle_start_pos.position
+			paul_boob.visible = false
+			kai_boob.visible = true
+			started = true
+		else:
+			if (Input.is_action_just_pressed("Enter")):
+				pierce = true
+			if (!pierce):
+				if (rise):
+					if (needle.position.y > needle_max_pos.position.y):
+						needle.position.y -= speed * delta
+					else:
+						rise = false
+						speed = randf_range(min_speed,max_speed)
+				else:
+					if (needle.position.y < needle_min_pos.position.y):
+						needle.position.y += speed * delta
+					else:
+						rise = true
+						speed = randf_range(min_speed,max_speed)
+			else:
+				if (needle.position.x > pierce_pos.position.x):
+						needle.position.x -= (speed * 10) * delta
+				else:
+					if(wait_after_pierce >= 0):
+						wait_after_pierce -= delta
+					else: 
+						if (!ok_shot && !great_shot):
+							pierce = false
+							needle.position = needle_start_pos.position
+							wait_after_pierce = buffer
+
+
+func _on_area_2d_area_entered(area):
+	if (started):
+		if (area.is_in_group("ok")):
+			ok_shot = true
+		if (area.is_in_group("great")):
+			great_shot = true
