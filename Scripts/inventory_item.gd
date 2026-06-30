22b8dd41c66fb7fd45902e28bc0defbab28608ce
chase562 <thedemondodge@gmail.com>
2026-06-30 13:44:19 -0400

    started working on inventory items
    todo to finish this feature
    
    PAUL thoughts
    Toggle ordering (what is focused and what isn't)

--- Scripts/inventory_item.gd
+++ Scripts/inventory_item.gd
@@ -1,6 +1,19 @@
 extends Button
+@export var anim_player : AnimationPlayer
+@export var mover : Node2D
 @export var item_name : String
-@export var item_spawn : String
-
+@export var item_unfurl : Sprite2D
+@export var thought_spawn_location : Node2D
+var show : bool = false
+var buffer_max = 1
+var buffer : float
 #if item spawns a response
 #if item spawns an inspectable
+func _process(delta: float) -> void:
+	if (!anim_player.is_playing()):
+		if(button_pressed && !show):
+			anim_player.play("mover_in")
+			show = true
+		if(!button_pressed && show):
+			anim_player.play("mover_out")
+			show = false
